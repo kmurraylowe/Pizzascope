172b2ec1ac55575bba0ee04b9fe0a76e468ca528
Kye Lowe <kmurraylowe@gmail.com>
2021-01-04 13:03:15 +1100

    first commit

---
+++ js/main.js
@@ -0,0 +1,204 @@
+document.querySelector("#date-btn").addEventListener("click", date)
+
+
+
+function date() {
+let month = document.querySelector("#date").value
+let date = document.querySelector("#day").value
+if (month == 12 && date >=22 || month == 1 && date <=19){
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.remove('hidden')
+}
+if (month == 1 && date >=20 || month == 2 && date <=18) {
+	console.log(day)
+	document.querySelector('#january').classList.remove('hidden')
+	document.querySelector("#febuary").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+}
+if (month == 1 && date > 31) {value = "Huh?";}
+if (month == 2 && date >=19 || month == 3 && date <=20) {
+	
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector('#febuary').classList.remove('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+	if (month == 2 && date > 29) {
+		alert("WRONG!")
+		document.querySelector('#febuary').classList.add('hidden')}
+}
+
+
+if (month == 3 && date >=21 || month == 4 && date <=19) {
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.remove('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+}
+
+
+if (month == 4 && date >=20 || month == 5 && date <=20) {
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.remove('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+	if (month == 4 && date > 30) {
+		alert("wrong")
+		document.querySelector("#april").classList.add('hidden')
+	}
+}
+if (month == 5 && date >=21 || month == 6 && date <=21) {
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.remove('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+}
+if (month == 6 && date >=22 || month == 7 && date <=22) {
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.remove('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+	if (month == 6 && date > 30) {
+		alert("WRONG")
+		document.querySelector("#june").classList.add('hidden')
+	}
+}
+if (month == 7 && date >=23 || month == 8 && date <=22)  {
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.remove('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+}
+if (month == 8 && date >=23 || month == 9 && date <=22) {
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.remove('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+}
+if (month == 9 && date >=23 || month == 10 && date <=22) {
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.remove('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+	if (month == 9 && date > 30) {
+		alert("WRONG")
+		document.querySelector("#september").classList.add('hidden')
+	}
+}
+if (month == 10 && date >=23 || month == 11 && date <=21) {
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.remove('hidden')
+	document.querySelector("#november").classList.add('hidden')
+	document.querySelector("#december").classList.add('hidden')
+}
+if (month == 11 && date >=22 || month == 12 && date <=21) {
+	document.querySelector('#febuary').classList.add('hidden')
+	document.querySelector("#january").classList.add('hidden')
+	document.querySelector("#march").classList.add('hidden')
+	document.querySelector("#april").classList.add('hidden')
+	document.querySelector("#may").classList.add('hidden')
+	document.querySelector("#june").classList.add('hidden')
+	document.querySelector("#july").classList.add('hidden')
+	document.querySelector("#august").classList.add('hidden')
+	document.querySelector("#september").classList.add('hidden')
+	document.querySelector("#october").classList.add('hidden')
+	document.querySelector("#november").classList.remove('hidden')
+	document.querySelector("#december").classList.add('hidden')
+	if (month == 11 && date > 30) {
+		alert("WRONG")
+		document.querySelector("#november").classList.add('hidden')
+	}
+}
+
+
+	}
